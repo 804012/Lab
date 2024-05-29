1ae122a94065914fc1b3b29750b9c6eba20e1272
Impostor from Among Us <143523357+804012@users.noreply.github.com>
2024-05-29 11:25:34 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private float speed = 20.0f;
+
+    void Update()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+}
